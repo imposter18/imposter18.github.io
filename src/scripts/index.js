aa5c1224d87991031da95d19d7280c6ec4b89ab3
ivan <redneckk@protonmail.com>
2020-10-24 21:38:08 +0300

    slick

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,22 @@
-123
+
+
+$(document).ready(function(){
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      vertical: true
+      
+    });
+  });
+  
